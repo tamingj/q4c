ae17375aaf690b12e3a871bbcf277f6f54a2039d
LeonStoldt <leon.stoldt@otto.de>
2023-11-03 15:33:45 +0100

    feat: Add supplier to ObjectSet instantiation.

--- src/main/java/io/github/cbuschka/objset/ObjectSet.java
+++ src/main/java/io/github/cbuschka/objset/ObjectSet.java
@@ -1,6 +1,8 @@
 package io.github.cbuschka.objset;
 
 import io.github.cbuschka.objset.impl.ObjectSetImpl;
+
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public interface ObjectSet {
@@ -10,6 +12,10 @@ public interface ObjectSet {
 
     static <Element> ObjectSet of(Class<Element> type, Iterable<Element> elements) {
         return new ObjectSetImpl().with(type, elements);
+    }
+
+    static <Element> ObjectSet of(Class<Element> type, Supplier<Iterable<Element>> elementSupplier) {
+        return new ObjectSetImpl().with(type, elementSupplier.get());
     }
 
     <Element> ObjectSet with(Class<Element> type, Stream<Element> elements);
