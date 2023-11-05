2ece7c96241431faea2a93cdb78cb4bcfef7f2c2
LeonStoldt <leon.stoldt@otto.de>
2023-11-05 21:03:28 +0100

    feat: Add iterable supplier to ObjectQuery construction.

--- src/main/java/io/github/cbuschka/objset/ObjectQuery.java
+++ src/main/java/io/github/cbuschka/objset/ObjectQuery.java
@@ -2,8 +2,14 @@ package io.github.cbuschka.objset;
 
 import io.github.cbuschka.objset.impl.UniSelectImpl;
 
+import java.util.function.Supplier;
+
 public class ObjectQuery {
     public static <Element1> FilterableUniSelect<Element1> selectFrom(Iterable<Element1> elements) {
         return new UniSelectImpl<>(elements);
+    }
+
+    public static <Element1> FilterableUniSelect<Element1> selectFrom(Supplier<Iterable<Element1>> elements) {
+        return new UniSelectImpl<>(elements.get());
     }
 }
